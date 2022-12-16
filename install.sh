b47b311ec34dbbdfeb9b4f10f0d92024687e48b8
Ba1_Ma0 <52622597+baimao-box@users.noreply.github.com>
2022-12-16 13:43:36 +0800

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,12 @@
+sudo apt update
+sudo apt install foremost
+sudo apt install binwalk
+sudo apt install exiftool
+sudo apt-get install python-tk
+sudo apt install strings
+sudo apt install xxd
+sudo curl https://bootstrap.pypa.io/pip/2.7/get-pip.py -o get-pip.py
+sudo python2 get-pip.py
+sudo pip2 install pillow
+sudo pip2 install matplotlib
+sudo pip2 install numpy
